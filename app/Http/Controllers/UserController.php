c7874165169399492a05b7ad036bb99d291e8e19
Georgy Alferov <mimirondakota@gmail.com>
2020-09-28 16:11:24 +0300

    Работа с новинками

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+/*
+    public function actionRegister(){
+        $result=false;
+        if (isset($_POST['submit'])){
+            $name=$_POST['name'];
+            $password=$_POST['password'];
+        }
+    }
+*/
 }
